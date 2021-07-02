a03d0e8a0511ce82bb639263d8266fd5812feb27
PaushaliBiswas <paushalibiswas09@gmail.com>
2021-07-02 22:50:52 +0530

    Exception handling front end

--- client/src/app/components/login/login.component.ts
+++ client/src/app/components/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { GoldPriceConstants } from 'src/app/constants/gold-price-constants';
 import { User } from 'src/app/models/user';
 import { AppService } from 'src/app/services/app.service';
 
@@ -14,18 +15,20 @@ export class LoginComponent {
   user: User;
   usernameError : string;
   passwordError : string;
+  loginError : string;
   
   loginUserDetails = new FormGroup({
     uname: new FormControl('', Validators.required),
     pwd: new FormControl('', Validators.required)
   });
   
-  constructor(private service: AppService, private router: Router) { }
+  constructor(private service: AppService, private router: Router, private constants : GoldPriceConstants) { }
 
   login(){
+    this.loginError = null;
     if(this.loginUserDetails.controls.uname.errors || this.loginUserDetails.controls.pwd.errors){
-      this.usernameError = this.loginUserDetails.controls.uname.errors ? 'Username is required!' : null;
-      this.passwordError = this.loginUserDetails.controls.pwd.errors ? 'Password is required!' : null;
+      this.usernameError = this.loginUserDetails.controls.uname.errors ? this.constants.userNameRequired : null;
+      this.passwordError = this.loginUserDetails.controls.pwd.errors ? this.constants.passwordRequired : null;
     }
     else{
       this.service.login(this.loginUserDetails.value.uname, this.loginUserDetails.value.pwd).subscribe(
@@ -33,12 +36,15 @@ export class LoginComponent {
           this.user = res;
           this.router.navigateByUrl('home/calculator');
         },
-        err => this.user.error = err
+        err => this.loginError = err.error.errorMsg
       )
     } 
   }
   reset(){
-
+    this.loginUserDetails.get("uname").reset('');
+    this.loginUserDetails.get("pwd").reset('');
+    this.usernameError = null;
+    this.passwordError = null;
   }
 
 }
